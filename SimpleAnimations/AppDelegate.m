15061eb3efac0f92d19fb34fd83531fbc96c3bb3
刘豪亮 <271072547@qq.com>
2016-08-15 16:53:03 +0800

    增加联系方式

--- SimpleAnimations/AppDelegate.m
+++ SimpleAnimations/AppDelegate.m
@@ -6,6 +6,19 @@
 //  Copyright © 2014年 刘豪亮. All rights reserved.
 //
 
+
+/*
+ *********************************************************************************
+ *
+ * ❤️❤️❤️ 新建SimpleAnimations交流QQ群：555041657 ❤️❤️❤️
+ *
+ * 在开发过程中有任何问题可以通过以下方式联系我，我会及时帮您解决问题。
+ * 新浪微博:HL_NetLover
+ * Email : 271072547@qq.com
+ * GitHub: https://github.com/MrUncle
+ *
+ */
+
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
--- SimpleAnimations/ViewController.m
+++ SimpleAnimations/ViewController.m
@@ -6,6 +6,18 @@
 //  Copyright © 2014年 刘豪亮. All rights reserved.
 //
 
+/*
+ *********************************************************************************
+ *
+ * ❤️❤️❤️ 新建SimpleAnimations交流QQ群：555041657 ❤️❤️❤️
+ *
+ * 在开发过程中有任何问题可以通过以下方式联系我，我会及时帮您解决问题。
+ * 新浪微博:HL_NetLover
+ * Email : 271072547@qq.com
+ * GitHub: https://github.com/MrUncle
+ *
+ */
+
 #define IMAGE1 @"01.jpg"
 #define IMAGE2 @"02.jpg"
 #define DURATION 0.7f
